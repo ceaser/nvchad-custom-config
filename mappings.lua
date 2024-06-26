9648a5afb8e2ed80ae6b607e42e48583c65f9d11
Ceaser Larry <clarry@divergentlogic.com>
2024-06-26 17:38:08 -0600

    Adding DAP with Ruby on Go

--- mappings.lua
+++ mappings.lua
@@ -7,13 +7,130 @@ M.general = {
   },
 }
 
--- Extras example
--- M.symbols_outline = {
---   n = {
---     ["<leader>cs"] = { "<cmd>SymbolsOutline<cr>", "Symbols Outline" },
---   },
--- }
-
--- more keybinds!
+-- Dap
+M.dap = {
+  n = {
+    ['<F5>'] = {
+      function()
+        require("dap").continue()
+      end,
+      "Start debugging"
+    },
+    ['<F10>'] = {
+      function()
+        require("dap").step_over()
+      end,
+      "Step over"
+    },
+    ['<F11>'] = {
+      function()
+        require("dap").step_into()
+      end,
+      "Step into"
+    },
+    ['<F12>'] = {
+      function()
+        require("dap").step_out()
+      end,
+      "Step out"
+    },
+    ['<Leader>.b'] = {
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      "Toggle breakpoint"
+    },
+    ['<Leader>.B'] = {
+      function()
+        require("dap").set_breakpoint()
+      end,
+      "Set breakpoint"
+    },
+    ['<Leader>.C'] = {
+      function()
+        require("dap").set_breakpoint(nil,nil, vim.fn.input('Breakpoint condition: '))
+      end,
+      "Set breakpoint Condition"
+    },
+    ['<Leader>.P'] = {
+      function()
+        require("dap").set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+      end,
+      "Set breakpoint w/ log"
+    },
+    ['<Leader>.dr'] = {
+      function()
+        require("dap").repl.open()
+      end,
+      "Open REPL"
+    },
+    ['<Leader>.dl'] = {
+      function()
+        require("dap").run_last()
+      end,
+      "Run last"
+    },
+    ['<Leader>.dt'] = {
+      function()
+        require("dap").debug_test()
+      end,
+      "Debug test"
+    },
+    ['<Leader>.dh'] = {
+      function()
+        require("dap.ui.widgets").hover()
+      end,
+      "Widget Hover"
+    },
+    ['<Leader>.dp'] = {
+      function()
+        require("dap.ui.widgets").preview()
+      end,
+      "Widget Preview"
+    },
+    ['<Leader>.df'] = {
+      function()
+        local widgets = require('dap.ui.widgets')
+        widgets.centered_float(widgets.frames)
+      end,
+      "Widget Center Frames"
+    },
+    ['<Leader>.ds'] = {
+      function()
+        local widgets = require('dap.ui.widgets')
+        widgets.centered_float(widgets.scopes)
+      end,
+      "Widget Center Scopes"
+    },
+  },
+  v = {
+    ['<Leader>.dh'] = {
+      function()
+        require("dap.ui.widgets").hover()
+      end,
+      "Widget Hover"
+    },
+    ['<Leader>.dp'] = {
+      function()
+        require("dap.ui.widgets").preview()
+      end,
+      "Widget Preview"
+    },
+    ['<Leader>.df'] = {
+      function()
+        local widgets = require('dap.ui.widgets')
+        widgets.centered_float(widgets.frames)
+      end,
+      "Widget Center Frames"
+    },
+    ['<Leader>.ds'] = {
+      function()
+        local widgets = require('dap.ui.widgets')
+        widgets.centered_float(widgets.scopes)
+      end,
+      "Widget Center Scopes"
+    },
+  },
+}
 
 return M
