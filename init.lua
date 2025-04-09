f2dd0d7d356514f7bf4f20cc9bb14e9556a150a0
Ceaser Larry <clarry@divergentlogic.com>
2025-04-09 02:50:56 -0600

    Trim trailing space

--- init.lua
+++ init.lua
@@ -1,10 +1,15 @@
 vim.opt.colorcolumn = "80"
 -- vim.g.copilot_assume_mapped = true
 
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+autocmd("BufWritePre", {
+  pattern = "*",
+  command = ":%s/\\s\\+$//e",
+})
